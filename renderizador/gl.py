9fa48d715b2e219c4fe8b6884d00b5a7754c67e1
EduardoMVAz <emendesvaz@gmail.com>
2025-08-19 23:03:59 -0300

    feat: Versão final entrega 1

--- renderizador/gl.py
+++ renderizador/gl.py
@@ -44,16 +44,20 @@ class GL:
         # O parâmetro colors é um dicionário com os tipos cores possíveis, para o Polypoint2D
         # você pode assumir inicialmente o desenho dos pontos com a cor emissiva (emissiveColor).
 
+        print(point)
+
         # Itera de dois em dois valores pelos pontos
         for i in range(0, len(point), 2):
+            print(i, len(point))
             # Formata os valores de x e y para o formato da função draw_pixel
-            point = [int(point[i]), int(point[i+1])]
+            cur_point = [int(point[i]), int(point[i+1])]
 
             # Formata as cores para o formato do frame_buffer
             color = [int(255 * colors['emissiveColor'][i]) for i in range(len(colors["emissiveColor"]))]
 
-            # E finalmente, desenha o pixel no Framebuffer
-            gpu.GPU.draw_pixel(point, gpu.GPU.RGB8, color)
+            # E finalmente, desenha o pixel no Framebuffer se ele se encontra na tela
+            if cur_point[0] >= 0 and cur_point[0] < GL.width and cur_point[1] >= 0 and cur_point[1] < GL.height:
+                gpu.GPU.draw_pixel(cur_point, gpu.GPU.RGB8, color)
         
     @staticmethod
     def polyline2D(lineSegments, colors):
@@ -112,15 +116,28 @@ class GL:
         # O parâmetro colors é um dicionário com os tipos cores possíveis, para o Circle2D
         # você pode assumir o desenho das linhas com a cor emissiva (emissiveColor).
 
-        print("Circle2D : radius = {0}".format(radius)) # imprime no terminal
-        print("Circle2D : colors = {0}".format(colors)) # imprime no terminal as cores
-        
-        # Exemplo:
-        pos_x = GL.width//2
-        pos_y = GL.height//2
-        gpu.GPU.draw_pixel([pos_x, pos_y], gpu.GPU.RGB8, [255, 0, 255])  # altera pixel (u, v, tipo, r, g, b)
-        # cuidado com as cores, o X3D especifica de (0,1) e o Framebuffer de (0,255)
+        # Extrai a cor
+        color = [int(255 * colors['emissiveColor'][i]) for i in range(len(colors["emissiveColor"]))]
 
+        # Define uma tolerância, dado que, para uma tela de pixels,
+        # é impossível ou muito raro que a distância euclidiana dos 
+        # pontos que formaram a circunferência seja exata ao
+        # calcular (x**2 + y**2) ** 0.5.
+        tolerance = 0.5 
+
+        # A ideia desse algoritmo é iterar pelo quadrado 
+        # formado pelo raio, e toda vez que a distância
+        # euclidiana calculada pros pontos x e y da origem
+        # for menor do que a tolerância (o correto seria ser zero
+        # mas dado a tela de pixels, precisamos de uma tolerância)
+        # pintamos o pixel.
+        for x in range(round(radius)+1): 
+            for y in range(round(radius)+1): 
+                
+                radial_distance = math.sqrt(y**2 + x**2) 
+
+                if abs(radial_distance - radius) <= tolerance: 
+                    gpu.GPU.draw_pixel([x, y], gpu.GPU.RGB8, color)
 
     @staticmethod
     def triangleSet2D(vertices, colors):
